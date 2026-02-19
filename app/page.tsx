64c916d468de709d1eccae78ccf2d3c4818a73ae
saurabh <saurabhyadav42399@gmail.com>
2026-02-19 23:47:16 +0530

    update landing page with new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
-
 'use client';
 
 import React from 'react';
 import { Hero } from './components/Hero';
+import { LiveMarketsStrip } from './components/LiveMarketsStrip';
+import { HowItWorks } from './components/HowItWorks';
 import { FeaturesSection } from './components/FeaturesSection';
+import { UseCases } from './components/UseCases';
+import { NumbersStrip } from './components/NumbersStrip';
 import { PageLayout } from './components/PageLayout';
 
 export default function Home() {
   return (
     <PageLayout>
       <Hero />
+      <LiveMarketsStrip />
+      <HowItWorks />
       <FeaturesSection />
+      <UseCases />
+      <NumbersStrip />
     </PageLayout>
   );
 }
