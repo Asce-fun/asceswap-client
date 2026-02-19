d70e324a96f895d3f92cbfd0994ed4fef1762161
saurabh <saurabhyadav42399@gmail.com>
2026-02-19 23:46:53 +0530

    register new protocol logos in dapp logo map

--- app/lib/helpers/dappLogos.tsx
+++ app/lib/helpers/dappLogos.tsx
@@ -3,6 +3,10 @@ import { ProtocolSymbol } from "../../interface/types";
 import EkuboLogo from "../../assets/icons/dapps/ekubo";
 import VesuLogo from "../../assets/icons/dapps/vesu";
 import NostraLogo from "../../assets/icons/dapps/nostra";
+import EndureFiLogo from "../../assets/icons/dapps/endurefi";
+import TrovesLogo from "../../assets/icons/dapps/troves";
+import USLogo from "../../assets/icons/dapps/us";
+import ParadexLogo from "../../assets/icons/dapps/paradex";
 
 export const PROTOCOL_LOGOS: Partial<
   Record<ProtocolSymbol, React.FC<{ size?: number }>>
@@ -10,4 +14,9 @@ export const PROTOCOL_LOGOS: Partial<
   Ekubo: ({ size = 16 }) => <EkuboLogo size={size} />,
   Vesu: ({ size = 16 }) => <VesuLogo size={size} />,
   Nostra: ({ size = 16 }) => <NostraLogo size={size} />,
+  "Endure.Fi": ({ size = 16 }) => <EndureFiLogo size={size} />,
+  Troves: ({ size = 16 }) => <TrovesLogo size={size} />,
+  US: ({ size = 16 }) => <USLogo size={size} />,
+  Paradex: ({ size = 16 }) => <ParadexLogo size={size} />,
+  Asceswap: ({ size = 16 }) => <VesuLogo size={size} />,
 };
